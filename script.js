97c2a96add207621dee94fe874c71774e9133898
dictedicte <dictethaysen@gmail.com>
2025-12-17 03:36:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,6 @@ function createCard(item) {
   li.className = "card";
   li.innerHTML = `
     <article>
-        <time datetime="20${item.year}-${item.month}-${item.day} ${item.time}">${item.time ? `${item.time}` : ""}</time>
         <h5>${item.description}</h5>
         ${item.title ? `<p>${item.title}</p>` : ""}
     </article>
